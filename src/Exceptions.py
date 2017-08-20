641c2fc44473163be8a1f2c75a5f56c38ec67efa
Junmo Kang <rapier83@gmail.com>
2017-08-21 03:24:09 +0900

    Add attribute `Players` by Pandas DataFrame Initializing time is dramatically down on 1.00+e07

--- src/Exceptions.py
+++ src/Exceptions.py
@@ -12,7 +12,17 @@ class isInteger(Exception):
 class isRate(Exception):
 
     def __init__(self):
-        self.val = "[Setting Error]: This Value Must Be An Integer."
+        self.val = "[Setting Error]: This Value Must Be 0 < Value < 1"
 
     def __str__(self):
         return self.val
+
+
+class isBool(Exception):
+
+    def __init__(self):
+        self.val = "[Setting Error]: This Value Must Be A Boolean"
+
+    def __str__(self):
+        return self.val
+
